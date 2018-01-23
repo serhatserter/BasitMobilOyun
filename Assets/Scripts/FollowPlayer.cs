d14a50b853ff3c3194cf936aad09ec9b75bcdadc
serhatserter <serhatserter@yandex.com>
2018-01-23 19:15:37 +0300

    BasitOyun

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,42 @@
+﻿
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+
+	public Transform player;
+	private new Vector3 camera;
+	public Quaternion baslangic;
+	float camDonus;
+	public bool sifirla,durdur;
+
+
+	private void Start()
+	{
+		camera = transform.position;
+		camDonus = 3f;
+		baslangic = transform.rotation;
+		sifirla = false;
+		durdur = false;
+			
+	}
+
+	void Update()
+	{
+		transform.position = player.position + camera;
+
+		if (durdur == false) { Hareket(); }
+		if (sifirla == true)
+		{
+			transform.rotation = baslangic;
+			sifirla = false;
+		}
+		
+	}
+
+
+	void Hareket()
+	{
+		transform.Rotate(0, 0, (Input.acceleration.x) / camDonus);
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+	public Rigidbody rb;
+	public float hiz,yanHiz;
+	float basa, dususHiz,dustu;
+	public bool yerde;
+	Vector3 baslama, yurume, yereDegme;
+	public Vector3 jumpPosition;
+	private new GameObject camera;
+	private FollowPlayer script;
+	float jumpSpeed,yukseklik;
+	public bool jump;
+
+	// Use this for initialization
+	void Start () {
+		hiz = 50f;
+		yanHiz = 50f;
+
+		yerde = false;
+		basa = -10f;
+
+		dususHiz = -25f;
+		dustu = 0.1f;
+
+		jumpSpeed = 30f;
+		jump = false;
+		yukseklik = 7f;
+
+		yurume = new Vector3(0, dususHiz, hiz);
+		baslama = transform.position;
+
+		camera = GameObject.FindWithTag("MainCamera");
+		script = camera.GetComponent<FollowPlayer>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate() {
+
+		if (yerde == true)
+		{
+			Hareket();
+			YanaHareket();	
+			DustuMu();		
+		}
+
+		Jump();
+		BasaDon();
+
+	}
+
+	void Hareket()
+	{
+		rb.velocity = yurume;
+		yurume.y = dususHiz;
+	}
+
+	void YanaHareket()
+	{	
+			yurume.x = Input.acceleration.x * yanHiz;	
+	}
+
+	void DustuMu()
+	{
+		if (yereDegme.y - dustu >= transform.position.y)
+		{
+			yerde = false;
+			jump = false;
+			rb.velocity = new Vector3(0, dususHiz, 0);
+		}
+	}
+
+	void BasaDon()
+	{
+		if (transform.position.y <= baslama.y + basa)
+		{
+			transform.position = baslama;
+			script.sifirla = true;
+			jump = false;
+		}
+	}
+
+	void Jump()
+	{
+		if (transform.position.y >= jumpPosition.y + yukseklik)
+		{
+			jump = false;
+			rb.velocity = new Vector3(yurume.x, -jumpSpeed, yurume.z);
+		}
+
+
+		if (Input.GetMouseButtonDown(0) && yerde==true){
+			jumpPosition = transform.position;
+			
+			jump = true;
+			
+		}
+
+		if (jump == true) {
+
+			rb.velocity=new Vector3(yurume.x, jumpSpeed, yurume.z);
+			yerde = false;
+			script.durdur = true;
+		}
+	}
+
+
+	void OnCollisionEnter(Collision col)
+	{
+		if (col.gameObject.tag == "Yer")
+		{
+			yerde = true;
+			jump = false;
+			script.durdur = false;
+			yereDegme = transform.position;
+			
+		}
+		
+	}
+}
